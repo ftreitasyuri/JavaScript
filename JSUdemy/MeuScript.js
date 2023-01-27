9a0def71b81454efd9c1d3084feed7daf6ba6583
Yuri Freitas Queiroz <yuri.queiroz@totvs.com.br>
2023-01-27 12:02:56 -0300

    Fazendo upload do projeto da Udemy

---
+++ JSUdemy/MeuScript.js
@@ -0,0 +1,10 @@
+
+
+// alert("Seja bem vindo ao meu curso de JS");
+
+// var texto = "Curso de Java Script"
+// forma de escrever no input via JS
+// document.getElementById('nome').value = texto
+
+document.getElementById('nome').value = 'Oi'
+
